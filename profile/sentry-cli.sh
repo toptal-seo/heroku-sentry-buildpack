dc22b7c6f9cfb34a5b4486ff0c21c1ea58df4cca
Alexander Makeev <alexmak@MacBook-Pro-Alexander.local>
2021-12-02 23:15:47 +0500

    Up

--- profile/sentry-cli.sh
+++ profile/sentry-cli.sh
@@ -1,6 +1,17 @@
-export SENTRY_ENVIRONMENT=$(cat "$HOME/vendor/env/SENTRY_ENVIRONMENT");
+#export SENTRY_ENVIRONMENT=$(cat "$HOME/vendor/env/SENTRY_ENVIRONMENT");
 export PATH="$PATH:$HOME/vendor/bin"
 
-echo "Setting hook"
-eval "$(sentry-cli bash-hook)"
-echo "Hook set."
+echo "Exporting environment variables"
+export_env_dir() {
+  env_dir=$1
+  acceptlist_regex=${2:-''}
+  denylist_regex=${3:-'^(PATH|GIT_DIR|CPATH|CPPATH|LD_PRELOAD|LIBRARY_PATH)$'}
+  if [ -d "$env_dir" ]; then
+    for e in $(ls $env_dir); do
+      echo "$e" | grep -E "$acceptlist_regex" | grep -qvE "$denylist_regex" &&
+      export "$e=$(cat $env_dir/$e)"
+      :
+    done
+  fi
+}
+export_env_dir "$HOME/vendor/env"
