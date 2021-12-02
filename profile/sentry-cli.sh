01b452e5702f3c11812874ac6852c750b288b0e6
Alexander Makeev <alexmak@MacBook-Pro-Alexander.local>
2021-12-02 22:56:09 +0500

    Up

--- profile/sentry-cli.sh
+++ profile/sentry-cli.sh
@@ -1,4 +1,6 @@
 export SENTRY_ENVIRONMENT=$(cat "$HOME/vendor/env/SENTRY_ENVIRONMENT");
 export PATH="$PATH:$HOME/vendor/bin"
 
-eval "$(sentry-cli bash-hook)"
+echo "Setting hook"
+#eval "$(sentry-cli bash-hook)"
+echo "Hook set."
